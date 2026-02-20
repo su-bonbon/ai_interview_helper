4bbaf2e9fe60b17096688f7f2bd8681345b5d026
Sujin <sujinbonnaud@gmail.com>
2026-02-20 14:39:44 -0600

    env setup

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="app">
+      <div className="card">
+        <h1>Vite + React + TS</h1>
+        <p>빠르게 시작할 준비 완료.</p>
+        <ul>
+          <li>Hot Reload</li>
+          <li>TypeScript</li>
+          <li>Vite 빌드</li>
+        </ul>
+      </div>
+    </div>
+  )
+}
+
+export default App
